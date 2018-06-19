8b77a05ab7c9d1e75ff838fc3ddad59be01cf16d
Julien Mattiussi <julienm@marmelab.com>
2018-06-19 17:11:40 +0200

    review

--- quarto/src/Api/GameApi.php
+++ quarto/src/Api/GameApi.php
@@ -2,54 +2,54 @@
 namespace App\Api;
 
 use App\Entity\Game;
+use App\Api\Piece;
 
 class GameApi {
 
-  public function __construct() {
+    public function __construct() {
 
-  }
-
-  public function new(int $size) : Game {
-    $grid = [];
-
-    for ($i = 0; $i < $size; $i++) {
-        $grid[$i] = [];
-        for ($j = 0; $j < $size; $j++) {
-            $grid[$i][$j] = '.';
-        }
     }
-    return new Game(1, $grid, true, 0);
-  }
 
-  public function getAllPieces(Game $game) : Array {
-    $pieces = [];
-    $size = count($game->getGrid());
+    function new(int $size) : Game {
+        $grid = [];
 
-    if ($size > 0) {
-        for ($i = 1; $i <= $size * $size; $i++) {
-            $pieces[$i] = new Piece($i, false);
-            foreach($game->getGrid() as $raw) {
-                foreach($raw as $item) {
-                    if ($item == $i) {
-                        $pieces[$i]->setUsed(true);
+        for ($i = 0; $i < $size; $i++) {
+            $grid[$i] = [];
+            for ($j = 0; $j < $size; $j++) {
+                $grid[$i][$j] = '.';
+            }
+        }
+        return new Game(1, $grid, true, 0);
+    }
+
+    function getAllPieces(Game $game) : Array {
+        $pieces = [];
+        $size = count($game->getGrid());
+
+        if ($size > 0) {
+            for ($i = 1; $i <= $size * $size; $i++) {
+                $pieces[$i] = new Piece($i, false);
+                foreach($game->getGrid() as $raw) {
+                    foreach($raw as $item) {
+                        if ($item === $i) {
+                            $pieces[$i]->setUsed(true);
+                        }
                     }
                 }
             }
         }
+        return $pieces;
     }
-    return $pieces;
-  }
 
-  public function changeTurn(Game $game) : Game {
-    $nextGame = clone $game;
-    $nextGame->setIsPlayerOneTurn(!$nextGame->getIsPlayerOneTurn());
-    return $nextGame;
-  }
+    function changeTurn(Game $game) : Game {
+        $nextGame = clone $game;
+        $nextGame->setIsPlayerOneTurn(!$nextGame->getIsPlayerOneTurn());
+        return $nextGame;
+    }
 
-  public function selectNextPiece(Game $game, int $id_piece) : Game {
-    $nextGame = clone $game;
-    $nextGame->setSelectedPiece($id_piece);
-    return $nextGame;
-  }
-
+    function selectNextPiece(Game $game, int $id_piece) : Game {
+        $nextGame = clone $game;
+        $nextGame->setSelectedPiece($id_piece);
+        return $nextGame;
+    }
 }
--- quarto/src/Repository/GameRepository.php
+++ quarto/src/Repository/GameRepository.php
@@ -1,21 +1,28 @@
 <?php
 namespace App\Repository;
+
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Game;
+
 class GameRepository extends EntityRepository {
+
   private $em;
+
   public function __construct(EntityManagerInterface $em) {
     $this->em = $em;
   }
+
   public function findGameById(string $id) : Game {
     return $this->em->find('App:Game', $id);
   }
+
   public function remove(string $id) {
     $game = $this->em->getReference('App:Game', $id);
     $this->em->remove($game);
     $this->em->flush();
   }
+  
   public function save(Game $game) {
     $this->em->persist($game);
     $this->em->flush();
