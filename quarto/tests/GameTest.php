9c10f5abb9b24214ff09fb00c254f7cf20c594d6
Julien Mattiussi <julienm@marmelab.com>
2018-06-19 15:54:02 +0200

    first test written

---
+++ quarto/tests/GameTest.php
@@ -0,0 +1,48 @@
+<?php
+namespace Tests\App\Entity;
+
+use PHPUnit\Framework\TestCase;
+use App\Entity\Game;
+
+class GameTest extends TestCase {
+
+  public function testSetIsPlayerOneTurn() {
+    $game = new Game(1,
+      array(
+        array(1, 2, 3),
+        array(4, 5, 6),
+        array(7, 8, 9)
+      ), true, 0);
+
+      $game->setIsPlayerOneTurn(false);
+    $this->assertEquals(false, $game->getIsPlayerOneTurn());
+  }
+
+  public function testSetSelectedPiece() {
+    $game = new Game(1,
+      array(
+        array(1, 2, 3),
+        array(4, 5, 6),
+        array(7, 8, 9)
+      ), true, 0);
+
+      $game->setSelectedPiece(6);
+    $this->assertEquals(6, $game->getSelectedPiece());
+  }
+
+  public function testSetGrid() {
+    $referenceArray = array(
+        array(1, 2),
+        array(3, 4));
+
+    $game = new Game(1,
+      array(
+        array(1, 2, 3),
+        array(4, 5, 6),
+        array(7, 8, 9)
+      ), true, 0);
+
+      $game->setGrid($referenceArray);
+    $this->assertEquals($referenceArray, $game->getGrid());
+  }
+}
